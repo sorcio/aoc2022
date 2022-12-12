5ceb00c6bc7058f50360737d4c2e29dea30435c2
Davide Rizzo <sorcio@gmail.com>
2022-12-13 00:36:19 +0100

    runner + template

--- aoc.py
+++ aoc.py
@@ -5,6 +5,7 @@ import requests
 
 INPUTS_DIR = Path("inputs")
 SESSION_PATH = Path(".sessionid")
+TEMPLATE_PATH = Path("aoc.py.template")
 
 
 def run_puzzle(puzzle: int, year: int, f: Callable[[IO[str]], None]) -> None:
@@ -14,6 +15,7 @@ def run_puzzle(puzzle: int, year: int, f: Callable[[IO[str]], None]) -> None:
 
 def puzzle(f: Callable[[IO[str]], None]) -> None:
     import inspect
+
     frame = inspect.currentframe()
     if frame is None:
         raise Exception("interpreter does not support frame inspection")
@@ -23,7 +25,24 @@ def puzzle(f: Callable[[IO[str]], None]) -> None:
 
     del frame
     del caller
-    
+
+    day, year = _get_day_and_year_from_path(path)
+
+    print(f"AOC {year} puzzle {day}")
+    print("Getting inputs...", end=" ", flush=True)
+    inputs = get_aoc_input(day, year)
+    print("done!")
+    print("Running puzzle code...")
+    print()
+    f(inputs)
+    print()
+    print("All done.")
+
+
+def _get_day_and_year_from_path(path: Path) -> tuple[int, int]:
+    # note: don't resolve() the path as we might be using symlinks for names
+    path = path.absolute()
+
     # very naive parser: we expect path to be `something/aocYEAR/dayN.py`
     try:
         day = int(path.name.removeprefix("day").removesuffix(".py"))
@@ -34,16 +53,8 @@ def puzzle(f: Callable[[IO[str]], None]) -> None:
         year = int(path.parent.name.removeprefix("aoc"))
     except ValueError:
         raise ValueError("only paths like `aoc2022/day12.py' are supported") from None
-    
-    print(f"AOC {year} puzzle {day}")
-    print("Getting inputs...", end=" ", flush=True)
-    inputs = get_aoc_input(day, year)
-    print("done!")
-    print("Running puzzle code...")
-    print()
-    f(inputs)
-    print()
-    print("All done.")
+
+    return day, year
 
 
 def get_aoc_input(puzzle: int, year: int) -> IO[str]:
@@ -71,6 +82,7 @@ def get_aoc_url(url: str) -> requests.Response:
 
 def _check_aoc_url(url: str):
     from urllib.parse import urlparse
+
     parsed = urlparse(url)
     if parsed.scheme != "https" and parsed.netloc != "adventofcode.com":
         raise Exception(f"URL is not from https://adventofcode.com/: {url!r}")
@@ -79,3 +91,33 @@ def _check_aoc_url(url: str):
 def get_cookies() -> dict[str, str]:
     session = SESSION_PATH.read_text().strip()
     return {"session": session}
+
+
+def main():
+    import sys
+
+    try:
+        puzzle = int(sys.argv[1])
+    except (ValueError, IndexError):
+        print(f"usage: python {sys.argv[0]} <PUZZLE_DAY>", file=sys.stderr)
+        sys.exit(1)
+
+    if not 1 <= puzzle <= 25:
+        print(f"{puzzle} is not a valid day for AOC", file=sys.stderr)
+        sys.exit(1)
+
+    path = Path(f"day{puzzle}.py")
+    day, year = _get_day_and_year_from_path(path)
+
+    if path.is_file():
+        import runpy
+
+        runpy.run_path(str(path))
+    else:
+        template = TEMPLATE_PATH.read_text()
+        path.write_text(template.format(DAY=day, YEAR=year))
+        print(f"{path} created from template. Run again to run puzzle.")
+
+
+if __name__ == "__main__":
+    main()
